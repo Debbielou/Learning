d9903e32ed712c6badfae02f6a144c4fa08b1d23
Debbielou <denakamatte@gmail.com>
2025-01-21 08:23:33 +0300

    Update springMvcSimple to Learning

---
+++ springMvcSimple/src/main/java/mvc/springMvcSimple/controller/PersonController.java
@@ -0,0 +1,16 @@
+package mvc.springMvcSimple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PersonController {
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		return "index.jsp";
+	}
+
+}
