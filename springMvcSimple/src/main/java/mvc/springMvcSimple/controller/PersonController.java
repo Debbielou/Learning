9c576a21c03e70a303b38f5c7a5bb0bda601bc38
Debbielou <denakamatte@gmail.com>
2025-01-23 08:20:31 +0300

    Update springMvcSimple in Learning

--- springMvcSimple/src/main/java/mvc/springMvcSimple/controller/PersonController.java
+++ springMvcSimple/src/main/java/mvc/springMvcSimple/controller/PersonController.java
@@ -1,6 +1,7 @@
 package mvc.springMvcSimple.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,6 +12,18 @@ public class PersonController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 		return "index.jsp";
+	}
+	
+	@RequestMapping(value = "/deb", method = RequestMethod.GET)
+	public String dbz() {
+		return "dbz";
+	}
+	
+	@RequestMapping(value = "/Good/morning", method = RequestMethod.GET)
+	public String greeting(Model model) {
+	    String hey = "Good morning my wonderful student, Debbie.";
+	    model.addAttribute("message", hey);
+	    return "greeting"; 
 	}
 
 }
